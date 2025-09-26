2906fa76dffab01840d1939dd613a7a156c53d88
Igor <igoryanec23@gmail.com>
2025-09-26 12:43:42 +0300

    Add tests for ParseTokenFromCookie function

--- internal/middlerware/auth_middle_test.go
+++ internal/middlerware/auth_middle_test.go
@@ -2,6 +2,8 @@ package middlerware
 
 import (
 	"errors"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 
@@ -10,7 +12,6 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
-//TO DO
 func GenerateToken(t *testing.T, sub string, exp time.Time) (string, error) {
     t.Helper()
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
@@ -51,10 +52,72 @@ func TestValidToken(t *testing.T) {
 
         err := ValidToken(token)
         if err != nil && !errors.Is(err, tt.expectedErr) {
-            t.Errorf("Unexpected error in test %v\n expected:%v but got:%v", tt.name, tt.expectedErr, err)
+            t.Fatalf("Unexpected error in test %v\nexpected:%v but got:%v", tt.name, tt.expectedErr, err)
         }
         if err == nil && tt.expectedErr != nil {
-            t.Errorf("Unexpected error in test %v\n expected:%v but got:%v", tt.name, tt.expectedErr, err)
+            t.Fatalf("Unexpected error in test %v\nexpected:%v but got:%v", tt.name, tt.expectedErr, err)
         }
     }
 }
+
+func TestParseTokenFromCookie(t *testing.T) {
+    tokenExp, _ := GenerateToken(t, "admin", time.Now().Add(time.Hour))
+    tests := []struct {
+        name string
+        addCookie bool
+        cookieName string
+        cookieVal string
+        expectedErr error
+    }{
+        {
+            name: "Valid",
+            addCookie: true,
+            cookieName: "AuthAdminToken",
+            cookieVal: tokenExp,
+            expectedErr: nil,
+        },
+        {
+            name: "Invalid Cookie name",
+            addCookie: true,
+            cookieName: "RandomName",
+            cookieVal: tokenExp,
+            expectedErr: http.ErrNoCookie,
+        },
+        {
+            name: "Empty Cookie name",
+            addCookie: true,
+            cookieName: "",
+            cookieVal: tokenExp,
+            expectedErr: http.ErrNoCookie,
+        },
+        {
+            name: "No Cookie",
+            addCookie: false,
+            cookieName: "",
+            cookieVal: "",
+            expectedErr: http.ErrNoCookie,
+        },
+    }
+
+    for _, tt := range tests {
+        req := httptest.NewRequest(http.MethodGet, "/", nil)
+        if tt.addCookie {
+            req.AddCookie(&http.Cookie{Name: tt.cookieName, Value: tt.cookieVal})
+        }
+
+        token, err := ParseTokenFromCookie(req) 
+
+        if err != nil && !errors.Is(err, tt.expectedErr) {
+            t.Fatalf("Unexpected error in test %v\nexpected:%v but got:%v", tt.name, tt.expectedErr, err)
+        }
+        if err == nil && tt.expectedErr != nil {
+            t.Fatalf("Unexpected error in test %v\nexpected:%v but got:%v", tt.name, tt.expectedErr, err)
+        }
+
+        if tt.expectedErr == nil && token != tt.cookieVal {
+            t.Fatalf("Test:%v\nWrong cookie value\nexpected cookie value: %v but got: %v", tt.name, tt.cookieVal, token)
+        }
+    }
+
+}
+
