583315eeedfabe97970cc5d3417757129af37e5c
Igor <igoryanec23@gmail.com>
2025-10-18 13:38:44 +0300

    rename page request.hmtl to status.html

--- internal/transport/handlers/user_handlers.go
+++ internal/transport/handlers/user_handlers.go
@@ -50,7 +50,7 @@ func (uh *UserHandler) formPage(w http.ResponseWriter, r *http.Request) {
 
 func (uh *UserHandler) statusPage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	path, err := filepath.Abs("web/request.html")
+	path, err := filepath.Abs("web/status.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
